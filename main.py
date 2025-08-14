68acd3ccb77c7671615415c31583bc51c1f93c3e
lpinon <lucas.pinon@cedrat-tec.com>
2025-08-14 10:44:15 +0200

    Création de l'architecture du projet

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-"""
-Simple script python permettant de :
-
-IHM ( réactif ?!!) permettant de communiquer avec un stm32 pilotant un moteur ( accelèrer, ralentir, arrêter...)
-Gestion des évenments par un système de logs
-Enregistrement dans une BDD type sql lite
-Afficher dans un graphe différentes courbes.
-
-"""
+""" Démarre l'application """""
---
+++ src/utils/logger.py
@@ -0,0 +1,12 @@
+"""
+Si c'est utilisé à plusieurs endroits ET que c'est générique  et indépendant
+utils/
+├── __init__.py
+├── helpers.py          ← Fonctions générales
+├── file_utils.py       ← Gestion fichiers
+├── math_utils.py       ← Calculs génériques
+├── validation.py       ← Validations
+├── logger.py           ← Configuration logs
+├── config.py           ← Gestion configuration
+└── constants.py        ← Constantes globales
+"""
