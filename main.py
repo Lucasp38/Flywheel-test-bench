e23bb753e1e16861c79b2685c6bc6dbcd92bc42d
lpinon <lucas.pinon@cedrat-tec.com>
2025-08-14 09:05:10 +0200

    Commit initial

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Simple script python permettant de :
+
+IHM ( réactif ?!!) permettant de communiquer avec un stm32 pilotant un moteur ( accelèrer, ralentir, arrêter...)
+Gestion des évenments par un système de logs
+Enregistrement dans une BDD type sql lite
+Afficher dans un graphe différentes courbes.
+
+"""
